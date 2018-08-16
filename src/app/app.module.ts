4a0553eeb9fd6e95c9b7acf30ddd54cf3d38cba1
Ian Odhiambo <you@example.com>
2018-08-16 21:19:52 +0300

    basic markup and syling for menu and profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
-import { RepoViewComponent } from './repo-view/repo-view.component';
-import { SearchComponent } from './search/search.component';
-import { MenuComponent } from './menu/menu.component';
+import { AppComponent } from "./app.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { RepoViewComponent } from "./repo-view/repo-view.component";
+import { SearchComponent } from "./search/search.component";
+import { MenuComponent } from "./menu/menu.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -15,10 +16,8 @@ import { MenuComponent } from './menu/menu.component';
     SearchComponent,
     MenuComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,17 @@
+import { GitService } from './../service/git.service';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
+  providers: [GitService],
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  constructor(private gitGitService: GitService) {}
 
   ngOnInit() {
+    // this.gitGitService;
   }
-
 }
