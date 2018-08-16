209f4a33c4dd5d5e229cf51716ef3f85ebc8157f
Ian Odhiambo <you@example.com>
2018-08-16 17:17:56 +0300

    preliminary set with profile,repo,menu,search components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RepoViewComponent } from './repo-view/repo-view.component';
+import { SearchComponent } from './search/search.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    RepoViewComponent,
+    SearchComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
