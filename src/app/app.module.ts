079984922e39115d089d605332791c3368670f58
Ian Odhiambo <you@example.com>
2018-08-23 04:48:41 +0300

    styled profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,6 @@ import { RepoViewComponent } from './repo-view/repo-view.component';
 import { SearchComponent } from './search/search.component';
 import { MenuComponent } from './menu/menu.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ProfileDetailsComponent } from './profile-details/profile-details.component';
-import { RepoViewDetailsComponent } from './repo-view-details/repo-view-details.component';
-
 @NgModule({
   declarations: [
     AppComponent,
